38b00a04c1dbd2b78aae3d508139be2def03f290
ianbdehaan <ianbertholdhaan@gmail.com>
2024-11-12 19:25:36 +0100

    manhattan idiots

--- ocaml-project/puzzle_game.ml
+++ ocaml-project/puzzle_game.ml
@@ -1,3 +1,11 @@
+let get_2d idx_1d n=
+  let x = (idx_1d mod n) in
+  let y = (idx_1d / n) in
+  [|x;y|]
+
+let get_1d idxs_2d n=
+  (idxs_2d.(0)+idxs_2d.(1)*n)
+
 let rec print_board n counter board=
   match board with
   | [] -> print_endline ""
@@ -33,99 +41,101 @@ let rec board_generator board nmbrs =
           (* add taken element to the board *)
           board_generator (drawn_val::board) new_nmbrs
 
-let rec switch_elements zero switch_number board = 
+let rec switch_elements switch_number board = 
   match board with 
   | [] -> []
-  | h::t -> if h = zero then switch_number::switch_elements zero switch_number t 
-  else if h = switch_number then zero::switch_elements zero switch_number t
-  else h::switch_elements zero switch_number t
+  | h::t -> if h = 0 then switch_number::switch_elements switch_number t 
+  else if h = switch_number then 0::switch_elements switch_number t
+  else h::switch_elements switch_number t
+
+let rec final_state board size counter =
+  match board with
+  | [] -> true
+  | h::t -> if h = counter then final_state t size (counter+1) 
+            else if h = 0 && counter = size then true
+            else false
 
 let rec get_idx number counter board =
   match board with
   | [] -> -1
   | h::t -> if h=number then counter else get_idx number (counter+1) t
 
-let check_move zero_idx offset n board =
-  let new_idx = zero_idx + offset in
-  if new_idx >= 0 && new_idx < n * n &&
-      ((offset = -1 &&  (zero_idx / n) = (new_idx / n)) || (* Left *)
-      (offset = 1 &&  (zero_idx / n) = (new_idx / n)) || (* Right *)
-      ((offset = -n) && (zero_idx mod n) = (new_idx mod n))|| (* Up *)
-      ((offset = n) &&  (zero_idx mod n) = (new_idx mod n)))(* Down *)
-  then
-    let switch_number = take_element new_idx 0 board in
-    switch_elements 0 switch_number board
-  else
-    (print_string "Move not possible\n"; board)
+let check_move zero_idxs movement n board =
+  let other_idxs =
+    match movement with
+    |"w" -> [|zero_idxs.(0); (zero_idxs.(1)+1)|]
+    |"s" -> [|zero_idxs.(0); (zero_idxs.(1)-1)|]
+    |"a" -> [|(zero_idxs.(0)+1); zero_idxs.(1)|]
+    |"d" -> [|(zero_idxs.(0)-1); zero_idxs.(1)|]
+    | _ -> [|zero_idxs.(0); zero_idxs.(1)|]
+    in
+    if ((other_idxs.(0)>=0) && (other_idxs.(1)>=0)) && ((other_idxs.(0)<n) && (other_idxs.(1)<n)) then 
+    let switch_number = (take_element (get_1d other_idxs n) 0 board) in
+      switch_elements switch_number board
+    else
+      board
 
+let invert_movement h=
+  if h = "w" then "s"
+  else if h="s" then "w"
+  else if h="d" then "a"
+  else "d"
+
+let rec manhattan_distance board counter n sum=
+  let calculate_one_distance idx nmbr=
+    let should_be_idxs_2d = get_2d (nmbr-1) n in
+    let idxs_2d = get_2d idx n in
+    abs (should_be_idxs_2d.(0)-idxs_2d.(0)) + abs (should_be_idxs_2d.(1) - idxs_2d.(1))
+  in
+  match board with
+  | [] -> sum
+  | h::t -> if h = 0 then manhattan_distance t (counter+1) n sum
+            else manhattan_distance t (counter+1) n (sum+(calculate_one_distance counter h))
+
+
+let rec play_game board size n moves_taken=
+  if final_state board size 1 = false then begin
+    print_string "which direction you want to move? Left (a), right(d), up (w), down (s)";
+    let choice = read_line () in 
+
+    let movement = 
+      if choice = "u" then 
+      match moves_taken with
+      | [] -> "x"
+      | h::t -> invert_movement h
+      else
+      choice
+    in
+
+    let new_moves_taken = 
+      if choice = "u" then 
+      match moves_taken with
+      | [] -> moves_taken
+      | h::t -> t
+      else
+      choice::moves_taken
+    in
+
+    let zero_idx = get_idx 0 0 board in 
+    let updated_board = check_move  (get_2d zero_idx n) movement n board in
+    print_board n 0 updated_board;
+    print_int (manhattan_distance updated_board 0 n 0);
+    play_game updated_board size n new_moves_taken
+    end
+  else
+    print_endline "you won the game, congratulations!"
 (* why () *)
+
 let () =
   print_string "Enter the size of the puzzle:";
   let n = int_of_string (read_line ()) in
-  let total_amount = n*n - 1 in  
-  Random.self_init ();
-  if total_amount >=0 then
+  if (n > 1) then
     (* makes a list with numbers 0-n *)
-    let nmbrs = build_nmbrs total_amount [] in
+    let nmbrs = build_nmbrs (n*n-1) [] in
     (* genertates board *)
     let board = (board_generator [] nmbrs) in
     print_board n 0 board; 
+    play_game board (n*n) n []
     (* take movement from user input *)
-    print_string "which direction you want to move? Left (l), right (r), up (u), down (d)";
-    let movement = read_line () in 
-    let zero_idx = get_idx 0 0 board in 
-    let updated_board = 
-      match movement with 
-      | "l" -> check_move zero_idx (1) n board
-      | "r" -> check_move zero_idx (-1) n board
-      | "u" -> check_move zero_idx (n) n board
-      | "d" -> check_move zero_idx (-n) n board
-      | _ -> (print_string "Not a valid move"; board)
-    in
-    print_board n 0 updated_board
   else
     print_string "Invalid input\n"
-
-
-    (* if movement = "l" then 
-      if (zero_idx / n) = ((zero_idx + 1) / n) && zero_idx + 1 < n*n then 
-        let switch_number = take_element (zero_idx+1) 0 board in 
-        let board = switch_elements 0 switch_number board in 
-        board 
-      else (print_string "Move not possible"; board);
-    else if movement = "r" then 
-      if (zero_idx / n) = ((zero_idx - 1) / n) && zero_idx - 1 > -1 then 
-        let switch_number = take_element (zero_idx-1) 0 board in 
-        let board = switch_elements 0 switch_number board in 
-        board 
-      else (print_string "Move not possible"; board);
-    else if movement = "u" then 
-      if (zero_idx mod n) = ((zero_idx - n) mod n) && zero_idx - n > -1 then 
-        let switch_number = take_element (zero_idx-n) 0 board in 
-        let board = switch_elements 0 switch_number board in 
-        board 
-      else (print_string "Move not possible"; board);
-    else if movement = "d" then 
-      if (zero_idx mod n) = ((zero_idx + n) mod n) && zero_idx + n < n*n then 
-        let switch_number = take_element (zero_idx+n) 0 board in 
-        let board = switch_elements 0 switch_number board in 
-        board 
-      else (print_string "Move not possible"; board)
-    else print_string "not a valid move";
-    print_board n 0 board 
-  
-  else
-    print_string "invalid input\n";
-  *)
-(* let return_1d x y n = 
-  if y * n + x > -1 & y * n + x < n * n then y * n + x else print_string "invalid input\n"; *)
-
-
-
-  (* print_string "Enter column of number you want to move";
-  let x = int_of_string (read_line ()) in
-  print_string "Enter row of number you want to move";
-  let y = int_of_string (readline ()) in 
-  let place_in_board = return_1d x y n in  *)
- 
-
